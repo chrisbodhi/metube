6e478eaa6e28ff723063091ac684b5ef83c63efb
chrisbodhi <cboette@gmail.com>
2013-12-04 13:17:37 -0600

    Add untested method for delete.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ MetubeCohort3::Application.routes.draw do
 
   root 'videos#show_all'
   get '/videos' => 'videos#show_all'
-  get '/videos/:id' => 'videos#show', as: 'video'
 
   get '/videos/:id/edit' => 'videos#edit', as: 'edit_video'
   put '/videos/:id' => 'videos#update'
   patch '/videos/:id' => 'videos#update'
 
+  get '/videos/:id' => 'videos#show', as: 'video'
+
+  delete '/videos/:id' => 'videos#destroy'
 end
